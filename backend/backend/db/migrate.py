f62899f435b82b2276fc6ccb2fc09f870fb18200
issacj-17 <68261814+issacj-17@users.noreply.github.com>
2025-01-11 11:49:48 +0800

    Update migrate.py
    - Amended variable to refer to Database URI stated in config.py

--- backend/backend/db/migrate.py
+++ backend/backend/db/migrate.py
@@ -1,10 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy import text
 import os
+from ...config import Config
 
-SQLALCHEMY_DATABASE_URL = 'mysql://root@localhost'
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
-engine = create_engine(SQLALCHEMY_DATABASE_URI, echo=True)
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI, echo=True)
 
 
 if __name__ == "__main__":
