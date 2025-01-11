cc42246569a7149c889e64ceb76535c56ac71921
Yamini Sezhiyan <yaminisezhiyan@gmail.com>
2025-01-11 15:26:08 +0800

    created the modal for create and edit button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,9 @@
-import { useContext } from "react"
+import { useContext,useState } from "react"
 import { CompanyContext } from "../contexts/company.context"
-import { Stack, Title, Blockquote, Flex, ActionIcon, Tooltip } from "@mantine/core"
+import {Button, Stack, Title, Blockquote, Flex, ActionIcon, Tooltip, Modal } from "@mantine/core"
 import OutstandingRequestsTable from "../components/OutstandingRequestsTable"
 import { IconCirclePlus } from "@tabler/icons-react"
+import CreateRequest from "../components/CreateRequest"
 
 const sampleOutstandingRequests = [
   {
@@ -33,6 +34,8 @@ const sampleOutstandingRequests = [
 
 export default function Home() {
   const { companyInfo } = useContext(CompanyContext)
+  const [editModalOpened, setEditModalOpened] = useState(true); // For Edit Modal
+  
 
   return (
     <>
@@ -51,14 +54,57 @@ export default function Home() {
           <Flex justify="space-between" align="center">
             <Title order={4}>Your outstanding requests</Title>
             <Tooltip label="Create a new request">
-              <ActionIcon color="teal" size="lg">
+              <ActionIcon color="teal" size="lg"
+              onClick={() => setEditModalOpened(true)}>
+
+
+
+          
                 <IconCirclePlus />
               </ActionIcon>
             </Tooltip>
+
+
+            <Modal
+            opened={editModalOpened}
+            onClose={() => setEditModalOpened(false)} // Close Modal
+            title="Create Request"
+            centered
+          >
+            <div>
+
+         
+
+              {/* Modal Content */}
+
+              <CreateRequest>
+       
+              <p>Create request here:</p>
+              <p>Request Date: </p>
+              <p>Company Name: </p>
+              <p>Carbon Price SGD/Tonnes:</p>
+              <p>Carbon Quantity: </p>
+              <p>Requesting Reason: </p>
+              <p>Request Type: </p>  
+
+              </CreateRequest>
+
+  
+              <Button
+                onClick={() => {
+                  console.log(`Edit request ${id}`);
+                  setEditModalOpened(false); // Close modal after action
+                }}
+              >
+                Save Changes
+              </Button>
+            </div>
+          </Modal>
           </Flex>
           <OutstandingRequestsTable requests={sampleOutstandingRequests}/>
         </Stack>
       </Stack>
+     
     </>
   )
 }
