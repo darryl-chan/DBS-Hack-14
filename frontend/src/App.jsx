3854fbc0c8f8cde40838a78ed70df69bf4cf1c8f
Yamini Sezhiyan <yaminisezhiyan@gmail.com>
2025-01-11 10:52:33 +0800

    added in login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { MantineProvider } from '@mantine/core';
 import Layout from './Layout'
 import Home from './pages/Home';
 import { Routes, Route, BrowserRouter } from 'react-router';
+import LoginPage from './components/LoginPage';
 
 function App() {
   return (
@@ -12,8 +13,10 @@ function App() {
           <Route path='/' element={<Layout/>}>
             <Route index element={<Home/>} />
           </Route>
+          <Route path='/login' element={<LoginPage/>} />
         </Routes>
       </BrowserRouter>
+     
     </MantineProvider>
   )
 }
