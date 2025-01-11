60a01867d9670c6294c6fcbb640401621f83cbcb
issacj-17 <68261814+issacj-17@users.noreply.github.com>
2025-01-11 13:44:41 +0800

    Update manager.py
    - Updated manager to interface with the techtrek2025 database properly
    - Amended bug in create engine method

--- backend/app/db/manager.py
+++ backend/app/db/manager.py
@@ -1,5 +1,3 @@
-
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 
@@ -11,13 +9,13 @@ class DatabaseManager:
         self.engine = None
         self.base = declarative_base()
 
-        def init_app(self, app):
-            self.create_engine
-            self.create_scoped_session()
-            self.base.query = self.session.query_property()
+    def init_app(self, app):
+        self.create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
+        self.create_scoped_session()
+        self.base.query = self.session.query_property()
 
     def create_engine(self, sqlalchemy_database_uri):
-        self.engine = create_engine(sqlalchemy_database_uri)
+        self.engine = create_engine(f"{sqlalchemy_database_uri}/techtrek2025")
 
     def create_scoped_session(self):
         self.session = scoped_session(
